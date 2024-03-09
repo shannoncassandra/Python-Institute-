85c25589c9b1b9590003a9df30748900db0e2ed1
shannoncassandra <61391254+shannoncassandra@users.noreply.github.com>
2024-03-08 19:41:35 -0800

    Update Triangle.py
    Adding more code that allows you to input the measurements of the triangle yourself

--- Triangle.py
+++ Triangle.py
@@ -12,3 +12,18 @@ def is_a_triangle(a, b, c):
 
 print(is_a_triangle(1, 1, 1))
 print(is_a_triangle(1, 1, 3))
+
+#Same thiing but you put in the values yourself!
+
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
